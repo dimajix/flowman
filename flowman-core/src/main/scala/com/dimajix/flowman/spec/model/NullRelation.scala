7aaaf5ca87173bd559f5f14499a268490aa6cd4c
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2018-03-31 11:51:55 +0200

    gitlab-23: Added Apache license

--- flowman-core/src/main/scala/com/dimajix/flowman/spec/model/NullRelation.scala
+++ flowman-core/src/main/scala/com/dimajix/flowman/spec/model/NullRelation.scala
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2018 Kaya Kupferschmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dimajix.flowman.spec.model
+
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.StructType
 
 import com.dimajix.flowman.execution.Executor
-import com.dimajix.flowman.spec.schema.SingleValue
 import com.dimajix.flowman.spec.schema.FieldValue
+import com.dimajix.flowman.spec.schema.SingleValue
 
 
 class NullRelation extends BaseRelation {
--- flowman-core/src/test/scala/com/dimajix/flowman/LocalSparkSession.scala
+++ flowman-core/src/test/scala/com/dimajix/flowman/LocalSparkSession.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Kaya Kupferschmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dimajix.flowman
 
 import java.io.File
@@ -11,9 +27,7 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.Suite
 import org.scalatest.mockito.MockitoSugar
 
-/**
-  * Created by kaya on 18.01.17.
-  */
+
 trait LocalSparkSession extends BeforeAndAfterAll  with MockitoSugar { this:Suite =>
     var spark: SparkSession = _
 

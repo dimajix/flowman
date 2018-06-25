ee6dc5f6416ec6ef54a6f412f87eb4f47d99a842
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2018-06-25 16:30:56 +0200

    gitlab-46 Made known-hosts optional and location configurable

--- flowman-core/src/main/scala/com/dimajix/flowman/spec/connection/SshConnection.scala
+++ flowman-core/src/main/scala/com/dimajix/flowman/spec/connection/SshConnection.scala
@@ -16,6 +16,8 @@
 
 package com.dimajix.flowman.spec.connection
 
+import java.io.File
+
 import com.fasterxml.jackson.annotation.JsonProperty
 
 import com.dimajix.flowman.execution.Context
@@ -27,11 +29,13 @@ class SshConnection extends Connection {
     @JsonProperty(value="keyPassword", required=false) private var _keyPassword:String = _
     @JsonProperty(value="username", required=true) private var _username:String = _
     @JsonProperty(value="password", required=false) private var _password:String = _
+    @JsonProperty(value="knownHosts", required=false) private var _knownHosts:String = _
 
     def host(implicit context: Context) : String = context.evaluate(_host)
     def port(implicit context: Context) : Int = Option(context.evaluate(_port)).map(_.toInt).getOrElse(22)
-    def keyFile(implicit context: Context) : String = context.evaluate(_keyFile)
+    def keyFile(implicit context: Context) : File = Option(context.evaluate(_keyFile)).filter(_.nonEmpty).map(f => new File(f)).orNull
     def keyPassword(implicit context: Context) : String = context.evaluate(_keyPassword)
     def username(implicit context: Context) : String  = context.evaluate(_username)
     def password(implicit context: Context) : String = context.evaluate(_password)
+    def knownHosts(implicit context: Context) : File = Option(context.evaluate(_knownHosts)).filter(_.nonEmpty).map(f => new File(f)).orNull
 }
--- flowman-core/src/main/scala/com/dimajix/flowman/spec/task/SftpUploadTask.scala
+++ flowman-core/src/main/scala/com/dimajix/flowman/spec/task/SftpUploadTask.scala
@@ -36,12 +36,12 @@ import com.dimajix.flowman.execution.Context
 import com.dimajix.flowman.execution.Executor
 import com.dimajix.flowman.fs.FileSystem
 import com.dimajix.flowman.spec.ConnectionIdentifier
-import com.dimajix.flowman.spec.connection
 import com.dimajix.flowman.spec.connection.SshConnection
 import com.dimajix.flowman.util.tryWith
 
 
 object SftpUploadTask {
+    private val logger = LoggerFactory.getLogger(classOf[SftpUploadTask])
     /**
       * The current implementation of @see KnownHosts does not support
       * known_hosts entries that use a non-default port.
@@ -50,6 +50,7 @@ object SftpUploadTask {
       */
     private class PortAwareKnownHosts(val knownHosts: File) extends KnownHosts(knownHosts) {
         def verifyHostkey(hostname: String, port: Int, serverHostKeyAlgorithm: String, serverHostKey: Array[Byte]): Int = {
+            logger.debug(s"Verifying remote server $hostname:$port with algorithm $serverHostKeyAlgorithm")
             val finalHostname = if (port != 22)
                 s"[$hostname]:$port"
             else
@@ -194,42 +195,63 @@ class SftpUploadTask extends BaseTask {
 
         logger.info(s"Connecting via SFTP to $host:$port")
         val connection = new Connection(host, port)
-        val knownHosts = new PortAwareKnownHosts(new File(System.getProperty("user.home") + "/.ssh/known_hosts"))
-        connection.connect(new ServerHostKeyVerifier {
-            @throws[Exception]
-            def verifyServerHostKey (hostname: String, port: Int, serverHostKeyAlgorithm: String, serverHostKey: Array[Byte]): Boolean = {
-                if (knownHosts.verifyHostkey(hostname, port, serverHostKeyAlgorithm, serverHostKey) == KnownHosts.HOSTKEY_IS_OK) {
-                    true
-                }
-                else {
-                    logger.error("Couldn't verify host key for " + hostname)
-                    throw new IOException("Couldn't verify host key for " + hostname)
-                }
-            }
-        })
+        connection.connect(hostKeyVerifier(credentials))
 
         if (password != null && password.nonEmpty) {
-            if (connection.isAuthMethodAvailable(username, "password")) connection.authenticateWithPassword(username, password)
-            else if (connection.isAuthMethodAvailable(username, "keyboard-interactive")) connection.authenticateWithKeyboardInteractive(username, new InteractiveCallback() {
-                @throws[Exception]
-                override def replyToChallenge(name: String, instruction: String, numPrompts: Int, prompt: Array[String], echo: Array[Boolean]): Array[String] = {
-                    prompt.length match {
-                        case 0 =>
-                            return new Array[String](0)
-                        case 1 =>
-                            return Array[String](password)
+            if (connection.isAuthMethodAvailable(username, "password")) {
+                logger.debug(s"Using non-interactive password authentication for connecting to $host:$port")
+                connection.authenticateWithPassword(username, password)
+            }
+            else if (connection.isAuthMethodAvailable(username, "keyboard-interactive")) {
+                logger.debug(s"Using interactive password authentication for connecting to $host:$port")
+                connection.authenticateWithKeyboardInteractive(username, new InteractiveCallback() {
+                    @throws[Exception]
+                    override def replyToChallenge(name: String, instruction: String, numPrompts: Int, prompt: Array[String], echo: Array[Boolean]): Array[String] = {
+                        prompt.length match {
+                            case 0 =>
+                                return new Array[String](0)
+                            case 1 =>
+                                return Array[String](password)
+                        }
+                        logger.error(s"Cannot proceed with keyboard-interactive authentication to $host:$port. Server requested " + prompt.length + " challenges, we only support 1.")
+                        throw new IOException(s"Cannot proceed with keyboard-interactive authentication to $host:$port. Server requested " + prompt.length + " challenges, we only support 1.")
                     }
-                    throw new IOException("Cannot proceed with keyboard-interactive authentication. Server requested " + prompt.length + " challenges, we only support 1.")
-                }
-            })
+                })
+            }
             else {
-                throw new IOException("Server does not support any of our supported password authentication methods")
+                logger.error(s"Server at $host:$port does not support any of our supported password authentication methods")
+                throw new IOException(s"Server at $host:$port does not support any of our supported password authentication methods")
             }
         }
         else {
-            connection.authenticateWithPublicKey(username, new File(keyFile), keyPassword)
+            logger.debug(s"Using private key authentication for connecting to $host:$port")
+            connection.authenticateWithPublicKey(username, keyFile, keyPassword)
         }
 
         connection
+    }
+
+    private def hostKeyVerifier(credentials:SshConnection)(implicit context: Context) : ServerHostKeyVerifier = {
+        val knownHosts = credentials.knownHosts
+        if (knownHosts != null) {
+            val verifier = new PortAwareKnownHosts(knownHosts)
+            new ServerHostKeyVerifier {
+                @throws[Exception]
+                def verifyServerHostKey(hostname: String, port: Int, serverHostKeyAlgorithm: String, serverHostKey: Array[Byte]): Boolean = {
+                    if (verifier.verifyHostkey(hostname, port, serverHostKeyAlgorithm, serverHostKey) == KnownHosts.HOSTKEY_IS_OK) {
+                        true
+                    }
+                    else {
+                        logger.error(s"Couldn't verify host key for $hostname:$port")
+                        throw new IOException(s"Couldn't verify host key for $hostname:$port")
+                    }
+                }
+            }
+        }
+        else {
+            new ServerHostKeyVerifier {
+                override def verifyServerHostKey(s: String, i: Int, s1: String, bytes: Array[Byte]): Boolean = true
+            }
+        }
     }
 }

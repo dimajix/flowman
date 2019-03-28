1a7cb1c9900935145b8fcc25540b1d88053f2fac
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2019-03-28 16:27:38 +0100

    Add support for writing SequenceFiles

---
+++ flowman-core/src/main/scala/com/dimajix/spark/sources/sequencefile/SequenceFileOptions.scala
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2018 Kaya Kupferschmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dimajix.spark.sources.sequencefile
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.io.BytesWritable
+import org.apache.hadoop.io.DoubleWritable
+import org.apache.hadoop.io.FloatWritable
+import org.apache.hadoop.io.IntWritable
+import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.NullWritable
+import org.apache.hadoop.io.ShortWritable
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.io.Writable
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
+import org.apache.spark.sql.types.BinaryType
+import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.types.FloatType
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.types.LongType
+import org.apache.spark.sql.types.NullType
+import org.apache.spark.sql.types.ShortType
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.unsafe.types.UTF8String
+
+import com.dimajix.flowman.hadoop.SerializableConfiguration
+
+
+case class WritableConverter(
+    writable:Class[_ <: org.apache.hadoop.io.Writable],
+    value:Class[_],
+    converter:Writable => Any,
+    writableExtractor:(InternalRow) => Writable,
+    writableFactory:() => Writable
+)
+
+
+object WritableConverter {
+    def of(fieldType:DataType, idx:Int) : WritableConverter = {
+        fieldType match {
+            case ShortType => WritableConverter(
+                classOf[ShortWritable],
+                classOf[Short],
+                (w:Writable) => w.asInstanceOf[ShortWritable].get(),
+                (row:InternalRow) => if (row.isNullAt(idx)) new ShortWritable() else  new ShortWritable(row.getShort(idx)),
+                () => new ShortWritable()
+            )
+            case IntegerType => WritableConverter(
+                classOf[IntWritable],
+                classOf[Int],
+                (w:Writable) => w.asInstanceOf[IntWritable].get(),
+                (row:InternalRow) => if (row.isNullAt(idx)) new IntWritable() else  new IntWritable(row.getInt(idx)),
+                () => new IntWritable()
+            )
+            case LongType => WritableConverter(
+                classOf[LongWritable],
+                classOf[Long],
+                (w:Writable) => w.asInstanceOf[LongWritable].get(),
+                (row:InternalRow) => if (row.isNullAt(idx)) new LongWritable() else  new LongWritable(row.getLong(idx)),
+                () => new LongWritable()
+            )
+            case FloatType => WritableConverter(
+                classOf[FloatWritable],
+                classOf[Float],
+                (w:Writable) => w.asInstanceOf[FloatWritable].get(),
+                (row:InternalRow) => if (row.isNullAt(idx)) new FloatWritable() else  new FloatWritable(row.getFloat(idx)),
+                () => new FloatWritable()
+            )
+            case DoubleType => WritableConverter(
+                classOf[DoubleWritable],
+                classOf[Double],
+                (w:Writable) => w.asInstanceOf[DoubleWritable].get(),
+                (row:InternalRow) => if (row.isNullAt(idx)) new DoubleWritable() else  new DoubleWritable(row.getDouble(idx)),
+                () => new DoubleWritable()
+            )
+            case StringType => WritableConverter(
+                classOf[Text],
+                classOf[String],
+                (w:Writable) => UTF8String.fromBytes(w.asInstanceOf[Text].getBytes),
+                (row:InternalRow) => if (row.isNullAt(idx)) new Text() else  new Text(row.getString(idx)),
+                () => new Text()
+            )
+            case BinaryType => WritableConverter(
+                classOf[BytesWritable],
+                classOf[Array[Byte]],
+                (w:Writable) => w.asInstanceOf[BytesWritable].copyBytes(),
+                (row:InternalRow) => if (row.isNullAt(idx)) new BytesWritable() else new BytesWritable(row.getBinary(idx)),
+                () => new BytesWritable()
+            )
+            case NullType => WritableConverter(
+                classOf[NullWritable],
+                classOf[Null],
+                (_:Writable) => null,
+                (_:InternalRow) => NullWritable.get(),
+                () => NullWritable.get()
+            )
+            case _ => throw new UnsupportedOperationException(s"Data type $fieldType not supported")
+        }
+    }
+}
+
+
+class SequenceFileOptions(
+        val config:SerializableConfiguration,
+         @transient val parameters: CaseInsensitiveMap[String],
+         val schema:StructType) extends Serializable {
+
+    if (schema.fields.length > 2)
+        throw new IllegalArgumentException("SequenceFiles can only contain up to two columns")
+
+    def this(config:Configuration, parameters: Map[String, String], schema:StructType) = {
+        this(new SerializableConfiguration(config), CaseInsensitiveMap(parameters), schema)
+    }
+
+    def hadoopConf : Configuration = config.value
+
+    def hasKey : Boolean = schema.fields.length > 1
+    def keyIdx : Int = if (hasKey) 0 else -1
+    def valueIdx  : Int = if (hasKey) 1 else 0
+
+    def keyType : DataType = if (hasKey) schema.fields(0).dataType else NullType
+    def valueType : DataType = schema.fields(valueIdx).dataType
+
+    def keyConverter : WritableConverter = WritableConverter.of(keyType, keyIdx)
+    def valueConverter : WritableConverter = WritableConverter.of(valueType, valueIdx)
+}
--- flowman-core/src/test/scala/com/dimajix/spark/sources/sequencefile/SequenceFileFormatTest.scala
+++ flowman-core/src/test/scala/com/dimajix/spark/sources/sequencefile/SequenceFileFormatTest.scala
@@ -16,6 +16,12 @@
 
 package com.dimajix.spark.sources.sequencefile
 
+import java.io.File
+
+import org.apache.spark.sql.types.BinaryType
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.functions.col
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
@@ -23,18 +29,152 @@ import com.dimajix.flowman.LocalSparkSession
 
 
 class SequenceFileFormatTest extends FlatSpec with Matchers with LocalSparkSession {
-    "A SequenceFile" should "be readable" in {
+    "A SequenceFile" should "be readable via load" in {
         val df = spark.read
             .format("sequencefile")
-            .load("lala")
+            .load(tempDir.toString)
         df should not be (null)
     }
 
-    "Multiple SequenceFiles" should "be readable" in {
+    it should "be readable via path option" in {
         val df = spark.read
             .format("sequencefile")
-            .option("path", "lala,lala2")
+            .option("path", tempDir.toString)
             .load()
         df should not be (null)
     }
+
+    it should "be writable and readable via save/load" in {
+        val spark = this.spark
+        import spark.implicits._
+
+        val dfOut = Seq(
+            ("key1", "value1"),
+            ("key2", "value2")
+        ).toDF
+
+        val tempFile = new File(tempDir, "seq1.seq")
+        dfOut.write
+            .format("sequencefile")
+            .mode("overwrite")
+            .save(tempFile.toString)
+
+        val dfIn = spark.read
+                .format("sequencefile")
+                .schema(dfOut.schema)
+                .load(tempFile.toString)
+        dfIn.schema.fields(0) should be (StructField("_1", StringType))
+        dfIn.schema.fields(1) should be (StructField("_2", StringType))
+
+        val rowsIn = dfIn.orderBy(col("_1")).collect.toSeq
+        val rowsOut = dfOut.orderBy(col("_1")).collect.toSeq
+        rowsIn should be (rowsOut)
+
+        tempFile.delete()
+    }
+
+    it should "be writable and readable via path option" in {
+        val spark = this.spark
+        import spark.implicits._
+
+        val dfOut = Seq(
+            ("key1", "value1"),
+            ("key2", "value2")
+        ).toDF
+
+        val tempFile = new File(tempDir, "seq1.seq")
+        dfOut.write
+            .format("sequencefile")
+            .mode("overwrite")
+            .option("path", tempFile.toString)
+            .save()
+
+        val dfIn = spark.read
+            .format("sequencefile")
+            .schema(dfOut.schema)
+            .option("path", tempFile.toString)
+            .load()
+        dfIn.schema.fields(0) should be (StructField("_1", StringType))
+        dfIn.schema.fields(1) should be (StructField("_2", StringType))
+
+        val rowsOut = dfOut.orderBy(col("_1")).collect.toSeq
+        val rowsIn = dfIn.orderBy(col("_1")).collect.toSeq
+        rowsIn should be (rowsOut)
+
+        tempFile.delete()
+    }
+
+    it should "be writable and readable without schema" in {
+        val spark = this.spark
+        import spark.implicits._
+
+        val dfOut = Seq(
+            ("key1", "value1"),
+            ("key2", "value2")
+        ).toDF.select(
+            col("_1").cast(BinaryType),
+            col("_2").cast(BinaryType)
+        )
+
+        val tempFile = new File(tempDir, "seq1.seq")
+        dfOut.write
+            .format("sequencefile")
+            .mode("overwrite")
+            .save(tempFile.toString)
+
+        val dfIn = spark.read
+            .format("sequencefile")
+            .load(tempFile.toString)
+        dfIn.schema.fields(0) should be (StructField("key", BinaryType))
+        dfIn.schema.fields(1) should be (StructField("value", BinaryType))
+
+        val rowsOut = dfOut.select(col("_1").cast(StringType), col("_2").cast(StringType)).orderBy(col("_1")).collect.toSeq
+        val rowsIn = dfIn.select(col("key").cast(StringType), col("value").cast(StringType)).orderBy(col("key")).collect.toSeq
+        rowsIn should be (rowsOut)
+
+        tempFile.delete()
+    }
+
+    it should "support reading and writing only values" in {
+        val spark = this.spark
+        import spark.implicits._
+
+        val dfOut = Seq(
+            ("value1"),
+            ("value2")
+        ).toDF
+
+        val tempFile = new File(tempDir, "seq1.seq")
+        dfOut.write
+            .format("sequencefile")
+            .mode("overwrite")
+            .save(tempFile.toString)
+
+        val dfIn = spark.read
+            .format("sequencefile")
+            .schema(dfOut.schema)
+            .load(tempFile.toString)
+        dfIn.schema.fields(0) should be (StructField("value", StringType))
+
+        val rowsOut = dfOut.orderBy(col("value")).collect.toSeq
+        val rowsIn = dfIn.orderBy(col("value")).collect.toSeq
+        rowsIn should be (rowsOut)
+
+        tempFile.delete()
+    }
+
+    "Multiple SequenceFiles" should "be readable via load" in {
+        val df = spark.read
+            .format("sequencefile")
+            .load(tempDir.toString, tempDir.toString)
+        df should not be (null)
+    }
+
+//    they should "be readable via path option" in {
+//        val df = spark.read
+//            .format("sequencefile")
+//            .option("path", tempDir.toString + "," + tempDir.toString)
+//            .load()
+//        df should not be (null)
+//    }
 }

e48d9b7b0db5eb60c6e9b95a14d534dc4dc40f2b
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2020-06-25 09:49:01 +0200

    Fix build for older Hadoop versions

--- flowman-core/src/main/scala/com/dimajix/flowman/types/AvroSchemaUtils.scala
+++ flowman-core/src/main/scala/com/dimajix/flowman/types/AvroSchemaUtils.scala
@@ -61,7 +61,7 @@ object AvroSchemaUtils {
     private def toAvro(field:Field, ns:String) : AField = {
         val schema = toAvro(field.ftype, ns, field.name, field.nullable)
         val default = toAvroDefault(field)
-        new AField(field.name, schema, field.description.orNull, default:Object)
+        new AField(field.name, schema, field.description.orNull, default)
     }
     private def toAvro(ftype:FieldType, ns:String, name:String, nullable:Boolean) : ASchema = {
         val atype = ftype match {

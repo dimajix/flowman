eea3717993549ded3748572fe815e55950663189
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2021-02-04 11:02:58 +0100

    gitlab-43 Interim commit

--- flowman-client/src/main/scala/com/dimajix/flowman/model/Profile.scala
+++ flowman-client/src/main/scala/com/dimajix/flowman/model/Profile.scala
@@ -1,24 +1,8 @@
-/*
- * Copyright 2018 Kaya Kupferschmidt
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.dimajix.flowman.spec
+package com.dimajix.flowman.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.JsonNode
 
+import com.dimajix.flowman.spec.Connection
 import com.dimajix.flowman.util.splitSettings
 
 
@@ -26,7 +10,7 @@ class Profile {
     @JsonProperty(value="enabled") private var _enabled : Boolean = true
     @JsonProperty(value="environment") private var _environment: Seq[String] = Seq()
     @JsonProperty(value="config") private var _config: Seq[String] = Seq()
-    @JsonProperty(value="connections") private var _databases: Map[String,JsonNode] = Map()
+    @JsonProperty(value="connections") private var _databases: Map[String,Connection] = Map()
 
     /**
       * Returns true if the profile is enabled
--- flowman-client/src/main/scala/com/dimajix/flowman/model/Project.scala
+++ flowman-client/src/main/scala/com/dimajix/flowman/model/Project.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.dimajix.flowman.spec
+package com.dimajix.flowman.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.JsonNode
@@ -22,37 +22,22 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.dimajix.flowman.util.splitSettings
 
 
-class Profile {
-    @JsonProperty(value="enabled") private var _enabled : Boolean = true
+class Project {
     @JsonProperty(value="environment") private var _environment: Seq[String] = Seq()
     @JsonProperty(value="config") private var _config: Seq[String] = Seq()
-    @JsonProperty(value="connections") private var _databases: Map[String,JsonNode] = Map()
+    @JsonProperty(value="profiles") private var _profiles: Map[String,Profile] = Map()
+    @JsonProperty(value="mappings") private var _mappings:Map[String,JsonNode] = Map()
+    @JsonProperty(value="relations") private var _relations:Map[String,JsonNode] = Map()
+    @JsonProperty(value="connections") private var _connections:Map[String,JsonNode] = Map()
+    @JsonProperty(value="outputs") private var _outputs:Map[String,JsonNode] = Map()
+    @JsonProperty(value="jobs") private var _jobs:Map[String,JsonNode] = Map()
 
-    /**
-      * Returns true if the profile is enabled
-      *
-      * @return
-      */
-    def enabled : Boolean = _enabled
-
-    /**
-      * Returns a map of all configured databases
-      *
-      * @return
-      */
-    def connections : Map[String,Connection] = _databases
-
-    /**
-      * Returns all configuration variables as a key-value sequence
-      *
-      * @return
-      */
+    def profiles : Map[String,Profile] = _profiles
+    def relations : Map[String,JsonNode] = _relations
+    def connections : Map[String,JsonNode] = _connections
+    def mappings : Map[String,JsonNode] = _mappings
+    def outputs : Map[String,JsonNode] = _outputs
+    def jobs : Map[String,JsonNode] = _jobs
     def config : Seq[(String,String)] = splitSettings(_config)
-
-    /**
-      * Returns the environment as a key-value-sequence
-      *
-      * @return
-      */
     def environment : Seq[(String,String)] = splitSettings(_environment)
 }

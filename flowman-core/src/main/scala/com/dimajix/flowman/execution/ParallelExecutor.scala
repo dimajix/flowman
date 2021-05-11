ba27e4ca2106015ccea43856319fab2906a289f3
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2021-05-11 09:53:00 +0200

    Fix race condition in ParallelExecutor

--- flowman-core/src/main/scala/com/dimajix/flowman/execution/ParallelExecutor.scala
+++ flowman-core/src/main/scala/com/dimajix/flowman/execution/ParallelExecutor.scala
@@ -18,7 +18,10 @@ package com.dimajix.flowman.execution
 
 import java.lang.Thread.UncaughtExceptionHandler
 import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.ForkJoinWorkerThread
+import java.util.concurrent.TimeUnit
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext
@@ -26,12 +29,9 @@ import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 import scala.util.Failure
 import scala.util.Success
-import java.util.concurrent.ForkJoinPool
-import java.util.concurrent.ForkJoinWorkerThread
 
 import org.slf4j.LoggerFactory
 
-import com.dimajix.common.IdentityHashMap
 import com.dimajix.flowman.config.FlowmanConf
 import com.dimajix.flowman.model.Target
 
@@ -75,64 +75,90 @@ class ParallelExecutor extends Executor {
         val threadPool = new ForkJoinPool(parallelism, MyForkJoinWorkerThreadFactory, exceptionHandler, true)
         implicit val ec:ExecutionContext = ExecutionContext.fromExecutorService(threadPool)
 
-        val tasks = IdentityHashMap[Target, Future[Status]]()
+        // Allocate state variables for tracking overall Status
+        val statusLock = new Object
         var error = false
         var skipped = true
         var empty = true
 
-        def executeTarget(target:Target) : Unit = {
-            val task = Future {
-                    fn(execution, target, phase)
-                }.andThen { case status =>
-                    // Inform scheduler that Target is built
-                    scheduler.synchronized {
-                        scheduler.complete(target)
-                    }
-
-                    // Remove task
-                    tasks.synchronized {
-                        tasks.remove(target)
-
-                        // Evaluate status (still in 'tasks' lock)
-                        status match {
-                            case Success(status) =>
-                                empty = false
-                                error |= (status != Status.SUCCESS && status != Status.SKIPPED)
-                                skipped &= (status == Status.SKIPPED)
-                                status
-                            case Failure(_) =>
-                                empty = false
-                                error = true
-                                Status.FAILED
-                        }
-                    }
+        def executeTarget(target:Target) : Future[Status] = {
+            Future {
+                fn(execution, target, phase)
+            }.andThen { case status =>
+                // Inform scheduler that Target is built
+                scheduler.synchronized {
+                    scheduler.complete(target)
                 }
 
-            // Append future to list of running futures
-            tasks.synchronized {
-                tasks.put(target, task)
+                // Evaluate status
+                statusLock.synchronized {
+                    status match {
+                        case Success(status) =>
+                            empty = false
+                            error |= (status != Status.SUCCESS && status != Status.SKIPPED)
+                            skipped &= (status == Status.SKIPPED)
+                            status
+                        case Failure(_) =>
+                            empty = false
+                            error = true
+                            Status.FAILED
+                    }
+                }
             }
         }
 
-        def scheduleTargets(): Unit = {
+        def scheduleTargets(): Seq[Future[Status]] = {
+            val tasks = mutable.ListBuffer[Future[Status]]()
             var noMoreWork = false
             while (!noMoreWork) {
                 scheduler.synchronized(scheduler.next()) match {
-                    case Some(target) => executeTarget(target)
-                    case None => noMoreWork = true
+                    case Some(target) =>
+                        val task = executeTarget(target)
+                        tasks.append(task)
+                    case None =>
+                        noMoreWork = true
                 }
+            }
+            tasks
+        }
+
+
+        @tailrec
+        def wait(tasks:Seq[Future[Status]]) : Unit = {
+            val runningTasks = tasks.filter(!_.isCompleted)
+            if (runningTasks.nonEmpty) {
+                val next = Future.firstCompletedOf(runningTasks)
+                Await.ready(next, Duration.Inf)
+                wait(tasks.filter(!_.isCompleted))
             }
         }
 
-        // Run all targets and schedule new targets as they become runnable
-        while(scheduler.synchronized(scheduler.hasNext()) && (!error || keepGoing)) {
-            scheduleTargets()
+        @tailrec
+        def run(tasks:Seq[Future[Status]] = Seq()) : Unit = {
+            // First wait for tasks
+            val (finishedTasks,runningTasks) = tasks.partition(_.isCompleted)
+            if (finishedTasks.isEmpty && runningTasks.nonEmpty) {
+                val next = Future.firstCompletedOf(runningTasks)
+                Await.ready(next, Duration.Inf)
+            }
 
-            val next = tasks.synchronized(Future.firstCompletedOf(tasks.values))
-            Await.ready(next, Duration.Inf)
+            // Schedule new Tasks
+            val newTasks = scheduleTargets()
+            val allTasks = runningTasks ++ newTasks
+            if(scheduler.synchronized(scheduler.hasNext()) && (!error || keepGoing)) {
+                run(allTasks)
+            }
+            else {
+                wait(allTasks)
+            }
         }
 
+        // Now schedule and execute all targets
+        run()
+
+        // Tidy up!
         threadPool.shutdown()
+        threadPool.awaitTermination(3600, TimeUnit.SECONDS)
 
         // Evaluate overall status
         if (empty)

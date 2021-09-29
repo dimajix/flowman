49a15a8b0cea00d91896e5f0850fafd045e9571b
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2021-09-29 10:35:57 +0200

    Fix failing tests

--- flowman-core/src/test/scala/com/dimajix/flowman/metric/MetricBoardTest.scala
+++ flowman-core/src/test/scala/com/dimajix/flowman/metric/MetricBoardTest.scala
@@ -49,7 +49,7 @@ class MetricBoardTest extends AnyFlatSpec with Matchers {
         val board = MetricBoard(context, Map("board_label" -> "board1", "status" -> "$status"), selections)
 
         board.metrics(registry, Status.RUNNING) should be (
-            Seq(FixedGaugeMetric("m1", Map("board_label" -> "board1", "rl" -> "raw_value", "sl" -> "a", "ev" -> "env_value", "status" -> "running"), 1l))
+            Seq(FixedGaugeMetric("m1", Map("board_label" -> "board1", "rl" -> "raw_value", "sl" -> "a", "ev" -> "env_value", "status" -> "RUNNING"), 1l))
         )
     }
 }

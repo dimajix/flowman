9493ecaaa7e91d75d5bb46727eb99d39b050bd03
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2021-10-27 15:29:25 +0200

    Improve building release versions with different profiles

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-rm -rf release
-mkdir release
+mkdir -p release
+
+FLOWMAN_VERSION=$(mvn -q -N org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -DforceStdout)
 
 build_profile() {
     profiles=""
@@ -9,17 +10,35 @@ build_profile() {
     do
         profiles="$profiles -P$p"
     done
-    mvn clean install $profiles -DskipTests -Ddockerfile.skip
+
+    # Set new version
+    HADOOP_DIST=$(mvn $profiles -q -N org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=hadoop.dist -DforceStdout)
+    SPARK_API_VERSION=$(mvn $profiles -q -N org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=spark-api.version -DforceStdout)
+    HADOOP_API_VERSION=$(mvn $profiles -q -N org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=hadoop-api.version -DforceStdout)
+
+    echo "Building for dist $HADOOP_DIST with Spark $SPARK_API_VERSION and Hadoop $HADOOP_API_VERSION"
+    mvn -q versions:set -DnewVersion=${FLOWMAN_VERSION}-${HADOOP_DIST}-spark${SPARK_API_VERSION}-hadoop${HADOOP_API_VERSION}
+
+    mvn clean install $profiles -DskipTests -Ddockerfile.skip -Dflowman.dist.suffix=""
     cp flowman-dist/target/flowman-dist-*.tar.gz release
+
+    # Revert to original version
+    mvn -q versions:revert
 }
 
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
 build_profile hadoop-2.6 spark-2.4
 build_profile hadoop-2.7 spark-2.4
+
+export JAVA_HOME=
 build_profile hadoop-2.7 spark-3.0
 build_profile hadoop-3.2 spark-3.0
 build_profile hadoop-2.7 spark-3.1
 build_profile hadoop-3.2 spark-3.1
 build_profile hadoop-2.7 spark-3.2
 build_profile hadoop-3.3 spark-3.2
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
 build_profile CDH-6.3
 build_profile CDP-7.1

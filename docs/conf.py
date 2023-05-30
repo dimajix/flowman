f61fc5aafbc9a87eb1d4d5187963fe7a164161f6
Siegfried Weber <mail@siegfriedweber.net>
2023-05-30 11:38:46 +0200

    Use the github-dark style for code blocks

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- General configuration ------------------------------------------------
 
@@ -80,7 +81,7 @@ language = 'en'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'style.FlowmanStyle'
+pygments_style = 'github-dark'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
--- docs/style.py
+++
@@ -1,4 +0,0 @@
-from pygments.styles.gh_dark import GhDarkStyle
-
-class FlowmanStyle(GhDarkStyle):
-    background_color = '#071f4e'
